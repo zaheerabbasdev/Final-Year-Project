0bff943de68347cdad498a87625539a6be14eb9e
Zaheer Abbas <zabbas092002@gmail.com>
2026-08-24 09:53:46 +0500

    add new ALB URL

--- mobile/lib/core/api_client.dart
+++ mobile/lib/core/api_client.dart
@@ -9,7 +9,7 @@ class ApiClient {
   // flutter run --dart-define=API_BASE_URL=http://<lan-ip-or-domain>:5000/api
   static const String baseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://academy-dev-alb-2776693.ap-south-1.elb.amazonaws.com/api',
+    defaultValue: 'http://academy-dev-alb-1048250030.ap-south-1.elb.amazonaws.com/api',
   );
 
   static final ApiClient _instance = ApiClient._internal();
